d43b74b16b45ef59dbf2808b521533a9d6eb3e19
Purav <mpurav2006@gmail.com>
2026-01-23 12:40:46 +0800

    Level-0: Rename chatbot and add greeting

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# QB User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/QB.java
@@ -0,0 +1,18 @@
+public class QB {
+    public static void main(String[] args) {
+        String logo = "  /$$$$$$   /$$$$$$$\n"
+                +" /$$__  $$ | $$__  $$\n"
+                +"| $$  \\ $$ | $$  \\ $$\n"
+                +"| $$  | $$ | $$$$$$$\n"
+                +"| $$  | $$ | $$__  $$\n"
+                +"| $$/$$ $$ | $$  \\ $$\n"
+                +"|  $$$$$$/ | $$$$$$$/\n"
+                +" \\____ $$$ |_______/\n"
+                +"      \\__/        ";
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm QB\nWhat can i do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
