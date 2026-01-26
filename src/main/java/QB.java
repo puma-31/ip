ac4b2d9c6bf74fca44333fc765b2bdcc25ce2ab4
Purav <mpurav2006@gmail.com>
2026-01-27 01:02:39 +0800

    Add echo functionality and bye command handling

--- src/main/java/QB.java
+++ src/main/java/QB.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class QB {
     public static void main(String[] args) {
         String logo = "  /$$$$$$   /$$$$$$$\n"
@@ -9,10 +11,28 @@ public class QB {
                 +"|  $$$$$$/ | $$$$$$$/\n"
                 +" \\____ $$$ |_______/\n"
                 +"      \\__/        ";
+
+        System.out.println(logo);
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm QB\nWhat can i do for you?");
+        System.out.println("Hello! I'm QB\nI can repeat whatever you say and i'll keep going till you say \"bye\".");
+        System.out.println("Go ahead, type something!");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        String inputLine;
+        Scanner in = new Scanner(System.in);
+
+        while (true){
+            inputLine = in.nextLine();
+
+            if (inputLine.equals("bye")){
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(inputLine);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
