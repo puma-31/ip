8d4da1c9828bff04765f73b20a8fbc7d5b929c75
Purav <mpurav2006@gmail.com>
2026-01-29 19:52:12 +0800

    Implement add and list commands

--- src/main/java/QB.java
+++ src/main/java/QB.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class QB {
+    private static final String LINE =
+            "____________________________________________________________";
     public static void main(String[] args) {
         String logo = "  /$$$$$$   /$$$$$$$\n"
                 +" /$$__  $$ | $$__  $$\n"
@@ -13,26 +15,46 @@ public class QB {
                 +"      \\__/        ";
 
         System.out.println(logo);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm QB\nI can repeat whatever you say and i'll keep going till you say \"bye\".");
-        System.out.println("Go ahead, type something!");
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
+        System.out.println("Hello! I'm QB\nI can add whatever you say to a list till you say \"bye\".");
+        System.out.println("Type \"list\" to view the items\nGo ahead, type something!");
+        System.out.println(LINE);
 
         String inputLine;
         Scanner in = new Scanner(System.in);
+
+        String[] itemList = new String[100];
+        int i = 0;
 
         while (true){
             inputLine = in.nextLine();
 
             if (inputLine.equals("bye")){
-                System.out.println("____________________________________________________________");
+                System.out.println(LINE);
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
+                System.out.println(LINE);
                 break;
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(inputLine);
-            System.out.println("____________________________________________________________");
+
+            if (inputLine.equals("list")){
+                System.out.println(LINE);
+                for (int j = 0; j< itemList.length; j++){
+                    if (itemList[j]!=null){
+
+                        System.out.println((j+1) + ". " + itemList[j]);
+
+                    }
+                }
+                System.out.println(LINE);
+            }
+
+            else {
+            itemList[i] = inputLine;
+            i++;
+            System.out.println(LINE);
+            System.out.println("added: " + inputLine);
+            System.out.println(LINE);
+            }
         }
     }
 }
