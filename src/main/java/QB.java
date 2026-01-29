84a59039cea371f8ff125661111403e3f85e1368
Purav <mpurav2006@gmail.com>
2026-01-30 03:08:48 +0800

    Implement mark and unmark commands for tasks

--- src/main/java/QB.java
+++ src/main/java/QB.java
@@ -9,25 +9,43 @@ public class QB {
         String inputLine;
         Scanner in = new Scanner(System.in);
 
-        String[] items = new String[100];
+        Task[] items = new Task[100];
         int i = 0;
 
         while (true){
             inputLine = in.nextLine();
+            String[] inputParts = inputLine.split(" ", 2);
+            String command = inputParts[0];
 
-            if (inputLine.equals("bye")){
-                printBye();
+            switch(command){
+            case "bye": printBye();
+                return;
+
+            case "list": printList(items);
+                break;
+
+            case "mark": if (inputParts.length < 2) {
+                System.out.println(LINE);
+                System.out.println("Please specify a task number.");
+                System.out.println(LINE);
                 break;
             }
+                markTask(items, Integer.parseInt(inputParts[1]));
+                break;
 
-            if (inputLine.equals("list")){
-                printList(items);
+            case "unmark": if (inputParts.length < 2) {
+                System.out.println(LINE);
+                System.out.println("Please specify a task number.");
+                System.out.println(LINE);
+                break;
             }
+                unmarkTask(items, Integer.parseInt(inputParts[1]));
+                break;
 
-            else {
-                items[i] = inputLine;
+            default: items[i] = new Task(inputLine);
                 i++;
                 printAdded(inputLine);
+                break;
             }
         }
     }
@@ -62,13 +80,53 @@ public class QB {
         System.out.println(LINE);
     }
 
-    private static void printList(String[] items) {
+    private static void printList(Task[] items) {
         System.out.println(LINE);
-        for (int i = 0; i < 100; i++) {
-            if (items[i] != null) {
-                System.out.println((i + 1) + ". " + items[i]);
-            }
+        int i = 0;
+        while (items[i] != null){
+            System.out.println((i + 1) + ". " + "[" + items[i].getStatusIcon() + "] "+ items[i].description);
+            i++;
         }
         System.out.println(LINE);
     }
+
+    private static void markTask(Task[] items, int itemNumber){
+        System.out.println(LINE);
+
+        if (itemNumber<1 || items[itemNumber-1] == null || itemNumber>100){
+            System.out.println("Please enter a valid number");
+        }
+
+        else if (!items[itemNumber-1].getStatusIcon().equals("X")){
+            items[itemNumber-1].markAsDone();
+            System.out.println("Nice! I've marked this task as done:\n\t[X] " + items[itemNumber-1].description);
+        }
+
+        else{
+            System.out.println("Oops! This task is already marked as done");
+        }
+
+        System.out.println(LINE);
+    }
+
+    private static void unmarkTask(Task[] items, int itemNumber){
+        System.out.println(LINE);
+
+        if (itemNumber<1 || items[itemNumber-1] == null || itemNumber>100){
+            System.out.println("Please enter a valid number");
+        }
+
+        else if (items[itemNumber-1].getStatusIcon().equals("X")){
+            items[itemNumber-1].unmarkAsDone();
+            System.out.println("Alright! I've unmarked this task as incomplete:\n\t[ ] " + items[itemNumber-1].description);
+        }
+
+        else{
+            System.out.println("Oops! This task is already marked as incomplete");
+        }
+
+        System.out.println(LINE);
+    }
+
+
 }
