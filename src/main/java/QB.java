72a280deb1883792b70bba2b0f25fd3fcad40a65
Purav <mpurav2006@gmail.com>
2026-01-29 21:40:45 +0800

    Refactor code by extracting helper methods

--- src/main/java/QB.java
+++ src/main/java/QB.java
@@ -3,7 +3,36 @@ import java.util.Scanner;
 public class QB {
     private static final String LINE =
             "____________________________________________________________";
+
     public static void main(String[] args) {
+        printGreeting();
+        String inputLine;
+        Scanner in = new Scanner(System.in);
+
+        String[] items = new String[100];
+        int i = 0;
+
+        while (true){
+            inputLine = in.nextLine();
+
+            if (inputLine.equals("bye")){
+                printBye();
+                break;
+            }
+
+            if (inputLine.equals("list")){
+                printList(items);
+            }
+
+            else {
+                items[i] = inputLine;
+                i++;
+                printAdded(inputLine);
+            }
+        }
+    }
+
+    public static void printGreeting(){
         String logo = "  /$$$$$$   /$$$$$$$\n"
                 +" /$$__  $$ | $$__  $$\n"
                 +"| $$  \\ $$ | $$  \\ $$\n"
@@ -13,48 +42,33 @@ public class QB {
                 +"|  $$$$$$/ | $$$$$$$/\n"
                 +" \\____ $$$ |_______/\n"
                 +"      \\__/        ";
-
         System.out.println(logo);
+
         System.out.println(LINE);
         System.out.println("Hello! I'm QB\nI can add whatever you say to a list till you say \"bye\".");
         System.out.println("Type \"list\" to view the items\nGo ahead, type something!");
         System.out.println(LINE);
+    }
 
-        String inputLine;
-        Scanner in = new Scanner(System.in);
+    public static void printBye(){
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
 
-        String[] itemList = new String[100];
-        int i = 0;
+    private static void printAdded(String item) {
+        System.out.println(LINE);
+        System.out.println("added: " + item);
+        System.out.println(LINE);
+    }
 
-        while (true){
-            inputLine = in.nextLine();
-
-            if (inputLine.equals("bye")){
-                System.out.println(LINE);
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(LINE);
-                break;
-            }
-
-            if (inputLine.equals("list")){
-                System.out.println(LINE);
-                for (int j = 0; j< itemList.length; j++){
-                    if (itemList[j]!=null){
-
-                        System.out.println((j+1) + ". " + itemList[j]);
-
-                    }
-                }
-                System.out.println(LINE);
-            }
-
-            else {
-            itemList[i] = inputLine;
-            i++;
-            System.out.println(LINE);
-            System.out.println("added: " + inputLine);
-            System.out.println(LINE);
+    private static void printList(String[] items) {
+        System.out.println(LINE);
+        for (int i = 0; i < 100; i++) {
+            if (items[i] != null) {
+                System.out.println((i + 1) + ". " + items[i]);
             }
         }
+        System.out.println(LINE);
     }
 }
