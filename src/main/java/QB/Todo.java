58f113dde8454f012241ff0c9a92a8f51524ec1f
Purav <mpurav2006@gmail.com>
2026-03-07 01:04:54 +0800

    Add JavaDoc to Todo.java

--- src/main/java/QB/Todo.java
+++ src/main/java/QB/Todo.java
@@ -1,10 +1,24 @@
 package QB;
 
+/**
+ * Represents a Todo task, which is a task with only a description and no time constraint.
+ */
 public class Todo extends Task {
+
+    /**
+     * Constructs a Todo task with the given description.
+     *
+     * @param description The description of the todo task.
+     */
     public Todo(String description) {
         super(description);
     }
 
+    /**
+     * Returns a string representation of the Todo task.
+     *
+     * @return A formatted string e.g. "[T][X] read book".
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
