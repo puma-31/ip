1692f9f5e3fc379dee73fbeefeee674c6c8fb23b
Purav <mpurav2006@gmail.com>
2026-03-07 01:05:22 +0800

    Add JavaDoc to Event.java

--- src/main/java/QB/Event.java
+++ src/main/java/QB/Event.java
@@ -1,24 +1,49 @@
 package QB;
 
+/**
+ * Represents an Event task, which is a task that occurs over a specific time period.
+ */
 public class Event extends Task {
     protected String from;
     protected String to;
 
+    /**
+     * Constructs an Event task with the given description, start time, and end time.
+     *
+     * @param description The description of the event.
+     * @param from The start time of the event.
+     * @param to The end time of the event.
+     */
     public Event(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
+    /**
+     * Returns a string representation of the Event task.
+     *
+     * @return A formatted string e.g. "[E][X] project meeting (from: Mon 2pm to: Mon 4pm)".
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 
+    /**
+     * Returns the start time of this event.
+     *
+     * @return The start time as a String.
+     */
     public String getFrom() {
         return from;
     }
 
+    /**
+     * Returns the end time of this event.
+     *
+     * @return The end time as a String.
+     */
     public String getTo() {
         return to;
     }
