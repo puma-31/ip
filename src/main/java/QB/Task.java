8cff8e149e491af45040ab5da5b1cd6f01e5f4eb
Purav <mpurav2006@gmail.com>
2026-03-07 01:04:36 +0800

    Add JavaDoc to Task.java

--- src/main/java/QB/Task.java
+++ src/main/java/QB/Task.java
@@ -1,26 +1,51 @@
 package QB;
 
+/**
+ * Represents a task with a description and completion status.
+ * This is the base class for all task types in the QB application.
+ */
 public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructs a Task with the given description, initially marked as not done.
+     *
+     * @param description The description of the task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns the status icon representing whether the task is done.
+     *
+     * @return "X" if the task is done, " " (space) if not.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void markAsDone() {
         isDone = true;
     }
 
+    /**
+     * Marks the task as not done.
+     */
     public void unmarkAsDone() {
         isDone = false;
     }
-    
+
+    /**
+     * Returns a string representation of the task, including its status and description.
+     *
+     * @return A formatted string e.g. "[X] read book".
+     */
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
