21cb9742531572ede97a4f6184e24ac019d71d05
Purav <mpurav2006@gmail.com>
2026-03-06 23:42:48 +0800

    Refactor QB to implement parsing logic

--- src/main/java/QB/QB.java
+++ src/main/java/QB/QB.java
@@ -1,168 +1,41 @@
 package QB;
 
-import java.util.ArrayList;
-
 public class QB {
 
-    private static Ui ui;
-    private static Storage storage;
-    private static TaskList tasks;
+    private Ui ui;
+    private Storage storage;
+    private TaskList tasks;
 
     private static final String FILE_PATH = "./data/QBList.txt";
 
-    public static void main(String[] args) throws QBException {
-
+    public QB(String filePath) {
         ui = new Ui();
-        ui.printGreeting();
-
-        storage = new Storage("./data/QBList.txt");
+        storage = new Storage(filePath);
         tasks = new TaskList();
-        ui.printTaskNumber(storage.loadTasks(tasks.getTasks()));
+        storage.loadTasks(tasks.getTasks());
+    }
+
+    public void run(){
+        ui.printGreeting();
+        ui.printTaskNumber(tasks.size());
 
         while (true) {
             String inputLine = ui.readCommand();
-            String[] inputParts = inputLine.split(" ", 2);
-            String command = inputParts[0];
-
-            if (command.equals("bye")) {
+            if (inputLine.trim().equals("bye")) {
                 ui.printBye();
                 ui.close();
                 return;
             }
 
             try {
-                handleCommand(command, inputParts, tasks);
+                Parser.parse(inputLine, tasks, ui, storage);
             } catch (QBException e) {
                 ui.printError(e.getMessage());
             }
         }
     }
 
-    private static void handleCommand(String command, String[] inputParts,
-                                     TaskList tasks) throws QBException {
-        switch (command) {
-        case "list":
-            ui.printList(tasks.getTasks());
-            break;
-
-        case "mark":
-            handleMarkCommand(inputParts, tasks);
-            storage.saveTasks(tasks.getTasks());
-            break;
-
-        case "unmark":
-            handleUnmarkCommand(inputParts, tasks);
-            storage.saveTasks(tasks.getTasks());
-            break;
-
-        case "delete":
-            handleDeleteCommand(inputParts, tasks);
-            storage.saveTasks(tasks.getTasks());
-            break;
-
-        case "todo":
-            handleTodoCommand(inputParts, tasks.getTasks());
-            storage.saveTasks(tasks.getTasks());
-            break;
-
-        case "deadline":
-            handleDeadlineCommand(inputParts, tasks.getTasks());
-            storage.saveTasks(tasks.getTasks());
-            break;
-
-        case "event":
-            handleEventCommand(inputParts, tasks.getTasks());
-            storage.saveTasks(tasks.getTasks());
-            break;
-
-        default:
-            throw new QBException("Sorry, I don't understand the command " + command);
-        }
-    }
-
-    private static void handleMarkCommand(String[] inputParts, TaskList tasks) throws QBException {
-        if (hasNoArguments(inputParts)) {
-            throw new QBException("Please specify a task number.");
-        }
-        try {
-            int taskNumber = Integer.parseInt(inputParts[1]);
-            tasks.markTask(taskNumber);
-            ui.printMarked(tasks.get(taskNumber - 1));
-        } catch (NumberFormatException e) {
-            throw new QBException("Task number must be a valid integer.");
-        }
-    }
-
-    private static void handleUnmarkCommand(String[] inputParts, TaskList tasks) throws QBException {
-        if (hasNoArguments(inputParts)) {
-            throw new QBException("Please specify a task number.");
-        }
-        try {
-            int taskNumber = Integer.parseInt(inputParts[1]);
-            tasks.unmarkTask(taskNumber);
-            ui.printUnmarked(tasks.get(taskNumber - 1));
-        } catch (NumberFormatException e) {
-            throw new QBException("Task number must be a valid integer.");
-        }
-    }
-
-    private static void handleDeleteCommand(String[] inputParts, TaskList tasks) throws QBException {
-        if (hasNoArguments(inputParts)) {
-            throw new QBException("Please specify a task number");
-        }
-
-        try {
-            int taskNumber = Integer.parseInt(inputParts[1]);
-            Task deletedTask = tasks.deleteTask(taskNumber);
-            ui.printDeleted(deletedTask, tasks.size());
-        } catch (NumberFormatException e) {
-            throw new QBException("Task number must be a valid integer.");
-        }
-    }
-
-    private static void handleTodoCommand(String[] inputParts, ArrayList<Task> tasks) throws QBException {
-        if (hasNoArguments(inputParts)) {
-            throw new QBException("Please provide a description for your Todo task");
-        }
-
-        tasks.add(new Todo(inputParts[1]));
-        ui.printAdded(tasks.get(tasks.size() - 1), tasks.size());
-    }
-
-    private static void handleDeadlineCommand(String[] inputParts, ArrayList<Task> tasks) throws QBException {
-        if (hasNoArguments(inputParts)) {
-            throw new QBException("Please provide a task description and deadline using /by.");
-        }
-
-        String[] deadlineParts = inputParts[1].split(" /by ", 2);
-        if (deadlineParts.length < 2) {
-            throw new QBException("Please use format: deadline <description> /by <time>");
-        }
-
-        tasks.add(new Deadline(deadlineParts[0], deadlineParts[1]));
-        ui.printAdded(tasks.get(tasks.size() - 1), tasks.size());
-    }
-
-    private static void handleEventCommand(String[] inputParts, ArrayList<Task> tasks) throws QBException {
-        if (hasNoArguments(inputParts)) {
-            throw new QBException("Please provide a task description, start and end time.");
-        }
-
-        String[] eventParts = inputParts[1].split(" /from ", 2);
-        if (eventParts.length < 2) {
-            throw new QBException("Please provide an event start time using /from");
-        }
-
-        String[] timeParts = eventParts[1].split(" /to ", 2);
-        if (timeParts.length < 2) {
-            throw new QBException("Please provide an event start time using /to");
-        }
-
-        tasks.add(new Event(eventParts[0], timeParts[0], timeParts[1]));
-        ui.printAdded(tasks.get(tasks.size() - 1), tasks.size());
-    }
-
-    private static boolean hasNoArguments(String[] inputParts) {
-        return inputParts.length < 2;
+    public static void main(String[] args) {
+        new QB(FILE_PATH).run();
     }
 }
